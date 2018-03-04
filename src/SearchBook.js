b75b79b59d6538a5164c21335c642bb6cebd7f88
BranDanny <zhangdeng1010@163.com>
2018-03-04 21:08:15 +0800

    modify

---
+++ src/SearchBook.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
+class SearchBook extends Component {
+	render() {
+		return <div>Hello Search</div>;
+	}
+}
+
+export default SearchBook;
